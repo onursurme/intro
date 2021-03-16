4cd5e3a0c1a261511acd01741c31a996871f8698
fatihkklnc <fatihkklnc@gmail.com>
2021-03-16 14:10:58 +0300

    axios post çalıştı

--- src/FormDemo1.js
+++ src/FormDemo1.js
@@ -1,13 +1,26 @@
 import React, { useEffect,useState } from 'react';
-import {Button,Form,FormGroup,Label,Input} from 'reactstrap';
+import {Button,Form,Label,Input, FormGroup} from 'reactstrap';
 import {useForm} from "react-hook-form";//use form kullanımı için form eklemesi yapılır
+import axios from 'axios';
 
 function FormDemo1(){
     const {register,handleSubmit,errors}=useForm();//oluşturulan form yapısını state oluşturup state e kaydediyor kolay form kullanımı için kullanılır
     
 
     const onSubmit=(event)=>{ 
-        alert(event.userName+event.city)
+        
+        var params={
+            id:event.id,
+            categoryName:event.categoryName,
+            seoUrl:event.seoUrl
+
+        };
+        axios.post('http://localhost:3000/categories',params).then((response)=>{
+            console.log(response)
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
     }
     
     /*const [state,setstate]= useState({userName:"",city:""})
@@ -33,10 +46,13 @@ function FormDemo1(){
                 
                 <form onSubmit={handleSubmit(onSubmit)} className="form-group">
                     
-                    <input name="userName"  type="text" className="form-control col-md-5 " ref={register}></input>
-                    <Label htmlFor="userName">User Name is:{}</Label>   
-                    <input name="city"  type="text" className="form-control col-md-5 " ref={register({required:true,minLength:3})}></input>
+                <label>city:{}</label>
+                    <input name="id"  type="text" className="form-control col-md-5 " ref={register}></input>
                     <label>city:{}</label> 
+                    <input name="categoryName"  type="text" className="form-control col-md-5 " ref={register({required:true,minLength:3})}></input>
+                    <label>city:{}</label>
+                    <input name="city"  type="text" className="form-control col-md-5 " ref={register()}></input>
+                    
                     {errors.city && <p>şehir 3 harften az olamaz</p>}   
                      <button type="submit"  className="btn btn-danger">save</button>   
                     
